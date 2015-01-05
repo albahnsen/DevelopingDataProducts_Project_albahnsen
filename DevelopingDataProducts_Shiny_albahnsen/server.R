4caebbdb38ea4b232db9ffe32f302e22fae7a88b
AlBahnsen <al.bahnsen@gmail.com>
2015-01-05 18:24:31 -0500

    new shiny app

--- DevelopingDataProducts_Shiny_albahnsen/server.R
+++ DevelopingDataProducts_Shiny_albahnsen/server.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(reshape2)
 library(randomForest)
 
+# Function to predict a score based on the previously calculated randomForests and a set of input features x
 predict_all <- function(classifiers, x){
   n_classifiers <- length(classifiers)
   n_obs <- dim(x)[1]
@@ -12,34 +13,40 @@ predict_all <- function(classifiers, x){
   for(i in 1:n_classifiers){
     p_hat[, i] <- predict(classifiers[[i]], x, type='prob')[,1]
   }
-  return (p_hat)
+  scores <- round((1-p_hat)*1000)
+  return (scores)
 }
 
 load('datasets.Rda')
 load('models.Rda')
 
-accept <<- 'init'
-
 shinyServer(
     function(input, output) {
+        
+        scores <- reactive({
+            
+            test = data[c(1),c(-1, -8)]
+            test$age = input$age
+            test$ncredits = input$ncredits
+            test$credithist = as.integer(input$credithist)
+            test$income = input$income
+            test$marital = input$marital
+            test$residence = input$residence
+            
+            p_test <- predict_all(cla, test)
+            
+            x <- melt(p_test)$value
+            x
+        })
+        
         output$newHist <- renderPlot({
-          
-          test = data[c(1),c(-1, -8)]
-          test$age = input$age
-          test$ncredits = input$ncredits
-          test$credithist = as.integer(input$credithist)
-          test$income = input$income
-          test$marital = input$marital
-          test$residence = input$residence
-          
+              
           # TODO: Add random user
           #input$goButton
           #  isolate(test = data[c(20080),c(-1, -8)])
 
-          p_test <- predict_all(cla, test)
-          
-          x <- melt(p_test)$value
-          xfit <-seq(0,1,length=1000)
+          x <- scores()
+          xfit <-seq(0,1000,length=1000)
           hx <- dnorm(xfit,mean=mean(x),sd=sd(x))
           df <- data.frame(xfit, hx)
           colnames(df) <- c('p_hat', 'pdist')
@@ -48,29 +55,29 @@ shinyServer(
           hx2 <- dnorm(x,mean=mean(x),sd=sd(x))
           df2 <- data.frame(x, hx2)
           colnames(df2) <- c('p_hat', 'pdist')
-          
-          # Adjust output
-          if (mean(x) <= 0.5) accept <<- "Decline Client"
-          else accept <<- "Accept Client"
 
           # Create Plot
             ggplot() + 
                 geom_point(data=df2, aes(x=p_hat, y=pdist), colour='turquoise4', size=3) +
                 geom_line(data=df, aes(x=p_hat, y=pdist), colour='firebrick4') + 
                 geom_vline(xintercept = mean(x), size=1.5, colour='grey44') +
-                geom_vline(xintercept = 0.5, linetype="dashed")
+                geom_vline(xintercept = 500, linetype="dashed") +
+                ylab(" ") +
+                xlab(" Risk Score") +
+                scale_x_continuous(breaks=seq(0,1000,100)) +
+                theme(axis.ticks = element_blank(), axis.text.y = element_blank())
         })
-        output$text1 <- renderPrint({          
-            
-            # Regardless of the change variable update accept
-            test = input$age
-            test = input$ncredits
-            test = as.integer(input$credithist)
-            test = input$income
-            test = input$marital
-            test = input$residence
-                                     
+        
+        output$text1 <- renderText({          
+            x <- scores()
+            if (mean(x) <= 500) accept <- "Declined"
+            else accept <- "Accepted"
             accept
+        })
+        
+        output$text_score <- renderText({ 
+            x <- scores()
+            paste(toString(round(mean(x))), '+/-', toString(round(sd(x))))
         })
         
     }
--- DevelopingDataProducts_Shiny_albahnsen/ui.R
+++ DevelopingDataProducts_Shiny_albahnsen/ui.R
@@ -2,19 +2,29 @@ library(shiny)
 shinyUI(pageWithSidebar(
     headerPanel("Credit risk assesment using public datasets"),
     sidebarPanel(
+        h4('Introduce the parameters to evaluate the scorecards'),
         sliderInput('age', 'Age',value = 40, min = 18, max = 80, step = 0.5,),
         sliderInput('ncredits', 'Number of credits',value = 1, min = 0, max = 10, step = 1,),
         p(' '),
-        checkboxInput("credithist", "Good credit history", FALSE),
-        checkboxInput("marital", "marital", FALSE),
-        checkboxInput("residence", "residence", FALSE),
-        radioButtons("income", "Income:", c("Low" = 1,"Mid" = 2, "High" = 3)),
-        actionButton("goButton", "Random Client")        
+        checkboxInput("credithist", "Good credit history?", FALSE),
+        checkboxInput("marital", "Marital status = Single?", FALSE),
+        checkboxInput("residence", "Own house?", FALSE),
+        radioButtons("income", "Income level:", c("Low" = 1,"Mid" = 2, "High" = 3)),
+        actionButton("goButton", "Random Input")        
     ),
     mainPanel(
-        p('Predicted risk'),
+        h3('Risk profile'),
+        p('Each point represents the estimated risk score using each one of the three databases. 
+          The higher the score the lower the risk. From a financial intitution perpective, customers
+          with a scorecard lower than 500 is not suitable for a loan.
+          '),
         plotOutput('newHist'),
-        textOutput('text1')
+        p('According to the input parameters the estimated score is:'),
+        textOutput('text_score'),
+        p('given the inputs a hipotetical loan will be:'),
+        textOutput('text1'),
+        h6('Disclaimer: The information on the Site is provided for educational or information purposes only; 
+           it is not intended to be a substitute for an actual credit risk (scorecard) estimator.')
     )
 ))
 
